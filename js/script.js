814fcbcd86ac51bf6262ab7807bb8888c128cb59
Stepan0202 <stepan020293@gmail.com>
2022-03-24 01:13:18 +0200

    Start of the logic. Add Solve button to interface

---
+++ js/script.js
@@ -0,0 +1,24 @@
+
+window.onload = init;
+
+function init(){
+    var button = document.getElementsByClassName("solve-button");
+    button[0].onclick = controller.solve;
+
+}
+
+var view = {
+    
+};
+
+var controller = {
+
+    solve: function(){
+        var 
+    }
+};
+
+var model = {
+    basicArray:
+    
+};
