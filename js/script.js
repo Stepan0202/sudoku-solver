d8ef2c54f200b9c065211f89c129f624a28bc078
Stepan0202 <stepan020293@gmail.com>
2022-03-24 21:06:15 +0200

    Add getModel function in object model. Function is getting sudoku array from the user interface

--- js/script.js
+++ js/script.js
@@ -8,17 +8,33 @@ function init(){
 }
 
 var view = {
-    
+    //disable all forms with validation of incoming data
 };
 
 var controller = {
 
-    solve: function(){
-        var 
+    solve : function(){
+        //send to view that we need to disable all forms
+        model.getModel();
+        //start solving in the model object
     }
 };
 
 var model = {
-    basicArray:
+   userArray: [["","","","","","","","",""],["","","","","","","","",""],["","","","","","","","",""],["","","","","","","","",""],["","","","","","","","",""],["","","","","","","","",""],["","","","","","","","",""],["","","","","","","","",""],["","","","","","","","",""]],
+
+   getModel: function(){
+       var cells = document.getElementsByClassName("cell");
+       console.log(cells[0].value)
+       console.log( this.userArray[0].length);
+       console.log( this.userArray.length);
+        for(var i = 0; i < this.userArray.length; i++){
+            for(var j = 0; j < this.userArray[i].length; j++){
+                var cellNumber = j + this.userArray.length*i;
+                this.userArray[i][j] = cells[cellNumber].value;
+            }
+        }
+       
+   }
     
 };
